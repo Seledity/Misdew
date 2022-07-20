a4829f3a48c54057a396476ffdf9669f752c3bf2
Justin <me@justa.us>
2022-07-20 09:15:23 -0400

    Create index.php

---
+++ app/ise-chat/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>iSE eChat - Misdew</title>
+  <meta charset="utf-8">
+  <meta name="description" content="We are a fairly cool social network.">
+  <meta name="keywords" content="Misdew, MD, Social, Network, Communication, 3DS, DSi, Nintendo">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="google" value="notranslate">
+  <meta name="theme-color" content="#1C1C1C">
+  <link rel="icon" type="image/png" href="/img/favicon.png">
+  <link rel="apple-touch-icon" href="/img/logo.png">
+  <style type="text/css">
+  body {
+    margin: 0;
+    background-color: #000;
+  }
+  .divs {
+    background-color: #1c1c1c;
+    border-bottom: 1px solid #424242;
+    width: 100%;
+    max-width: 500px;
+    padding-top: 5px;
+  }
+  .return_button {
+    width: 95%;
+    font-size: 18px;
+    background-color: #424242;
+    text-shadow: -2px 0px #000;
+    color: #fff;
+    border: 2px solid #000;
+  }
+  .input {
+
+  }
+  </style>
+</head>
+<body>
+  <center>
+    <div class="divs">
+      <div onclick="window.location='/hub';" class="return_button">
+        Return to Hub
+      </div>
+    </div>
+  </center>
+</body>
+</html>
