bd68ecd5bdd3cbd2be33c1bd45139d499ad860ad
Justin <me@justa.us>
2022-07-20 09:17:24 -0400

    Add files via upload

---
+++ app/justaboard/info.php
@@ -0,0 +1,33 @@
+<?php
+/*
+__  __ _____        _____
+|  \/  |  __ \      | ____|
+| \  / | |  | |_   _| |__
+| |\/| | |  | \ \ / /___ \
+| |  | | |__| |\ V / ___) |
+|_|  |_|_____/  \_/ |____/    Join us. The hype is real.
+
+You may code your application however you like.
+It may be designed however you like.
+You can do whatever you want, as long as you aren't mischievous.
+You will not be able to access anything from Misdew's database.
+However, you can connect to your own external database(s) if needed.
+Be sure to fill out the required info below.
+Once your app is done, zip all of your files into an archive.
+Be sure to also include this file in the archive.
+Name this archive as so: Username_AppName.zip
+Upload this archive to dev.misdew.com
+*/
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+ 	  # # # # # # # # # FILL OUT THE FORM # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+$application_name = "Justin Soundboard"; // Must not exceed 18 chars
+$application_icon = "https://i.imgur.com/i5QZ5yb.png"; // Upload your square, 1080x1080 icon to Imgur and paste the direct link here
+$application_desc = "Make Justin say what you want."; // Describe your app
+$application_price = "$1"; // Set the price of your app (0 to whatever you want; you keep 70%)
+$application_dev = "Joeyseph"; // Enter your Misdew username here
+$application_color = "green"; // Your app color; should be the same color as your icon's main color
+$application_tcolor = "black"; // Should look good on top of your app's color (is used for font)
+?>
