b861b1f6ae759fced707f8458705754e44222220
Justin <me@justa.us>
2022-07-21 07:18:44 -0400

    Add files via upload

--- removed_stuff/REMOVED_blogs/index.php
+++ removed_stuff/REMOVED_blogs/index.php
@@ -1,1 +1,73 @@
+<?php
+$this_page = "blogs";
+require_once("../inc/conx.php");
+if($logged_in == false) {
+  header("location: /");
+  exit();
+}
+#   #   #   #   #   #   #
+#    WEBSITE LOCATION   #
+#   #   #   #   #   #   #
+if($u_siteloc != '/blogs') {
+  $loc_desc = "viewin\' blogs";
+  mysqli_query($conx, "UPDATE accounts SET site_locdesc='$loc_desc' WHERE uid='$u_uid'");
+  mysqli_query($conx, "UPDATE accounts SET site_locurl='/blogs' WHERE uid='$u_uid'");
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Blogs - Misdew</title>
+  <meta charset="utf-8">
+  <meta name="description" content="We are a fairly cool social network.">
+  <meta name="keywords" content="Misdew, MD, Social, Network, Communication, 3DS, DSi, Nintendo">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="google" value="notranslate">
+  <meta name="theme-color" content="<?php echo $meta_theme_color; ?>">
+  <?php
+  if($css_type == "sheet") {
+    echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"$g_sheet\">";
+  }
+  if($css_type == "raw") {
+    echo "<style type=\"text/css\">$g_raw</style>";
+  }
+  ?>
 
+  <link rel="icon" type="image/png" href="/img/favicon.png">
+  <link rel="apple-touch-icon" href="/img/logo.png">
+  <style type="text/css">
+  body {
+    background-color: <?php echo $bgcolor; ?>;
+  }
+  #header_tds {
+    color: <?php echo $tdcolor; ?> !important;
+  }
+  </style>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-81238250-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-81238250-2');
+</script>
+
+</head>
+<body>
+  <center>
+    <?php
+    $back_button = true;
+    $linebreak = true;
+    $alerts = true;
+    require_once("../inc/header.php");
+    ?>
+    <span class="blogs_disc">
+        This app has yet to be developed due to lack of use in previous versions. <br>
+    </span>
+    <?php
+    require_once("../inc/footer.php");
+    ?>
+  </center>
+</body>
+</html>
